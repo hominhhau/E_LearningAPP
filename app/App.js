72bf6d809c3b4490b7ce0d628d6f21e2bd6f7008
Nguyen QuynhGiang <nguyenquynhgiang1324@gmail.com>
2024-11-04 15:29:57 +0700

    Change comment App.js

--- app/App.js
+++ app/App.js
@@ -41,64 +41,64 @@ import MyCourse from '../components/Screen/Main-Screens/MyCourse.tsx'
 //31.10.2024
 import ChatBubble from '../components/Screen/ChatBotScreen/ChatBubble.tsx'
 
-// export default function App() {
-//     return (
-//         // <ParallaxScrollView
-//         //   headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-//         //   headerImage={
-//         //     <Image
-//         //       source={require('@/assets/images/partial-react-logo.png')}
-//         //       style={styles.reactLogo}
-//         //     />
-//         //   }>
-//         //   <ThemedView style={styles.titleContainer}>
-//         //     <ThemedText type="title">Welcome!</ThemedText>
-//         //     <HelloWave />
-//         //   </ThemedView>
-//         //   <ThemedView style={styles.stepContainer}>
-//         //     <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-//         //     <ThemedText>
-//         //       Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-//         //       Press{' '}
-//         //       <ThemedText type="defaultSemiBold">
-//         //         {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-//         //       </ThemedText>{' '}
-//         //       to open developer tools.
-//         //     </ThemedText>
-//         //   </ThemedView>
-//         //   <ThemedView style={styles.stepContainer}>
-//         //     <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-//         //     <ThemedText>
-//         //       Tap the Explore tab to learn more about what's included in this starter app.
-//         //     </ThemedText>
-//         //   </ThemedView>
-//         //   <ThemedView style={styles.stepContainer}>
-//         //     <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-//         //     <ThemedText>
-//         //       When you're ready, run{' '}
-//         //       <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-//         //       <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-//         //       <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-//         //       <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-//         //     </ThemedText>
-//         //   </ThemedView>
-//         // </ParallaxScrollView>
+export default function App() {
+    return (
+        // <ParallaxScrollView
+        //   headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+        //   headerImage={
+        //     <Image
+        //       source={require('@/assets/images/partial-react-logo.png')}
+        //       style={styles.reactLogo}
+        //     />
+        //   }>
+        //   <ThemedView style={styles.titleContainer}>
+        //     <ThemedText type="title">Welcome!</ThemedText>
+        //     <HelloWave />
+        //   </ThemedView>
+        //   <ThemedView style={styles.stepContainer}>
+        //     <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        //     <ThemedText>
+        //       Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+        //       Press{' '}
+        //       <ThemedText type="defaultSemiBold">
+        //         {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
+        //       </ThemedText>{' '}
+        //       to open developer tools.
+        //     </ThemedText>
+        //   </ThemedView>
+        //   <ThemedView style={styles.stepContainer}>
+        //     <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+        //     <ThemedText>
+        //       Tap the Explore tab to learn more about what's included in this starter app.
+        //     </ThemedText>
+        //   </ThemedView>
+        //   <ThemedView style={styles.stepContainer}>
+        //     <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+        //     <ThemedText>
+        //       When you're ready, run{' '}
+        //       <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
+        //       <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
+        //       <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
+        //       <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+        //     </ThemedText>
+        //   </ThemedView>
+        // </ParallaxScrollView>
 
-//         //<WelcomeScreen />
-//         //<CreateNewPassword />
-//         //<ResetPassword />
-//         //<PasswordChanged />
-//         //<ForgotPassword />
-//         //<EnterCodeForgotPassword />
-//         //<Login />
-//         //<GoogleLogoIcon />
-//         //<Register />
-//         //<LessonNoCard />
-//         //<CreateAccountScreen />
-//         //<TeacherProfile />
-//         //<BenefitContainer />
-//         //<GeneralTabBar />
-//         //<CourseComponent />
+        //<WelcomeScreen />
+        //<CreateNewPassword />
+        //<ResetPassword />
+        //<PasswordChanged />
+        //<ForgotPassword />
+        //<EnterCodeForgotPassword />
+        //<Login />
+        //<GoogleLogoIcon />
+        //<Register />
+        //<LessonNoCard />
+        //<CreateAccountScreen />
+        //<TeacherProfile />
+        //<BenefitContainer />
+        //<GeneralTabBar />
+        //<CourseComponent />
 
         //<OverviewFinal />
         //<CategoryComponent />
@@ -111,12 +111,12 @@ import ChatBubble from '../components/Screen/ChatBotScreen/ChatBubble.tsx'
         //<RecommendedForYouComponent />
         //<TabBarMyCourse />
 
-//         //<HeaderMyCourse />
-//         //<HeaderAdvertisementCourse />
-//         //<MyCourse />
-//         //<CoursesProcessComponent />
+        //<HeaderMyCourse />
+        //<HeaderAdvertisementCourse />
+        //<MyCourse />
+        //<CoursesProcessComponent />
 
-//         //<FindComponent />
+        //<FindComponent />
 
         //<HotTopicComponent />
 
