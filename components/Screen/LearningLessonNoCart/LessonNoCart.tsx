a12fd149fa955ce7e6c340836839c3397272ba91
Nguyen QuynhGiang <nguyenquynhgiang1324@gmail.com>
2024-11-12 23:18:17 +0700

    update code switch tab

--- components/Screen/LearningLessonNoCart/LessonNoCart.tsx
+++ components/Screen/LearningLessonNoCart/LessonNoCart.tsx
@@ -1,96 +1,100 @@
-import React, { useState, useRef } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import TopNavigationBar from '@/components/navigation/TopNavigationBar';
-import TabBarNoCart from '../TabBar/TabBarNoCart';
-import LessonFinal from './EachTab/LessonFinal';
-import { Video } from 'expo-av';
-import YouTube from 'react-native-youtube-iframe';
-import { WebView } from 'react-native-webview';
+import React, { useState, useRef } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  ScrollView,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import TopNavigationBar from "@/components/navigation/TopNavigationBar";
+import TabBarNoCart from "../TabBar/TabBarNoCart";
+import LessonFinal from "./EachTab/LessonFinal";
+import OverviewFinal from "./EachTab/OverviewFinal";
+import { Video } from "expo-av";
+import YouTube from "react-native-youtube-iframe";
+import { WebView } from "react-native-webview";
 
-const LessonNoCart: React.FC = () =>{
-    const [activeTab, setActiveTab] = useState('LESSON');
-    const [selectedVideo, setSelectedVideo] = useState<string | null>(null); // Thay đổi kiểu dữ liệu
-    //const videoRef = useRef<Video>(null);
+const LessonNoCart: React.FC = () => {
+  const [activeTab, setActiveTab] = useState("LESSON");
+  const [selectedVideo, setSelectedVideo] = useState<string | null>(null); // Thay đổi kiểu dữ liệu
+  //const videoRef = useRef<Video>(null);
 
-    const handleVideoSelect = (videoLink: string) => {
-        setSelectedVideo(videoLink);
-    };
+  const handleVideoSelect = (videoLink: string) => {
+    setSelectedVideo(videoLink);
+  };
 
-    return(
-        <ScrollView contentContainerStyle={styles.scrollContainer}>
-        <View style={styles.container}>
-            <View style={styles.header}>
-            <TopNavigationBar 
-                onBackPress={() => {}}
-                onBookmarkPress={() => {}}
-                onMorePress={() => {}}
-                titleHeader="UX Foundation"
-            />
-            </View>
-
-            <View style={styles.thumbnailContainer}>
-            {selectedVideo && (
-                        <WebView
-                        source={{ uri: selectedVideo }}
-                        style={{ height: 200 }}
-                        javaScriptEnabled={true}
-                        domStorageEnabled={true}
-                        startInLoadingState={true}
-                        allowsFullscreenVideo={true}
-                    />
-                    )}
-            </View>
-
-            <View style={styles.courseInfoContainer}>
-                <Text>Course Info</Text>
-            </View>
-
-            <View style={styles.tabBarContainer}>
-                <TabBarNoCart activeTab={activeTab} setActiveTab={setActiveTab}/>
-                
-            </View>
-            <ScrollView contentContainerStyle={[styles.content, { paddingBottom: 80 }]}>
-               
-               {activeTab === 'LESSON' && (
-                   <View style={styles.lesson}>
-                       <LessonFinal onSelectVideo={handleVideoSelect} />
-                   </View>
-               )}
-           </ScrollView>
-
+  return (
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <TopNavigationBar
+            onBackPress={() => {}}
+            onBookmarkPress={() => {}}
+            onMorePress={() => {}}
+            titleHeader="UX Foundation"
+          />
         </View>
+
+        <View style={styles.thumbnailContainer}>
+          {selectedVideo && (
+            <WebView
+              source={{ uri: selectedVideo }}
+              style={{ height: 200 }}
+              javaScriptEnabled={true}
+              domStorageEnabled={true}
+              startInLoadingState={true}
+              allowsFullscreenVideo={true}
+            />
+          )}
+        </View>
+
+        <View style={styles.courseInfoContainer}>
+          <Text>Course Info</Text>
+        </View>
+
+        <View style={styles.tabBarContainer}>
+          <TabBarNoCart activeTab={activeTab} setActiveTab={setActiveTab} />
+        </View>
+        <ScrollView
+          contentContainerStyle={[styles.content, { paddingBottom: 80 }]}
+        >
+          {activeTab === "OVERVIEW" && (
+            <OverviewFinal />
+          )}
+          {activeTab === "LESSON" && (
+            <View style={styles.lesson}>
+              <LessonFinal onSelectVideo={handleVideoSelect} />
+            </View>
+          )}
         </ScrollView>
-    );
+      </View>
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
-    scrollContainer: {
-        paddingVertical: 20, // Tạo khoảng trống trên dưới khi cuộn
-    },
-    container:{
-        flex: 1,
-    },
-    header:{
-        
-    },
-    thumbnailContainer:{
-        //backgroundColor: 'pink',
-        height: 250,
-    },
-    courseInfoContainer:{
-        padding: 10,
-    },
-    tabBarContainer:{
-        padding: 10,
-    },
-    content:{
-
-    },
-    lesson:{
-            
-        },
-
+  scrollContainer: {
+    paddingVertical: 20, // Tạo khoảng trống trên dưới khi cuộn
+  },
+  container: {
+    flex: 1,
+  },
+  header: {},
+  thumbnailContainer: {
+    //backgroundColor: 'pink',
+    height: 250,
+  },
+  courseInfoContainer: {
+    padding: 10,
+  },
+  tabBarContainer: {
+    padding: 10,
+  },
+  content: {},
+  lesson: {},
 });
 
 export default LessonNoCart;
