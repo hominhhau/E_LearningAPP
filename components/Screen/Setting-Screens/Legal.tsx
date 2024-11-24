d8d18702a4a5e559fe304a59c72b38ca054ab62c
Nguyen QuynhGiang <nguyenquynhgiang1324@gmail.com>
2024-11-25 02:11:38 +0700

    Navigation setting

--- components/Screen/Setting-Screens/Legal.tsx
+++ components/Screen/Setting-Screens/Legal.tsx
@@ -25,7 +25,7 @@ const Legal: React.FC = () => {
       </View>
       <View style={styles.termContainer}>
         <Text style={styles.textTitle}>Term</Text>
-        <Text style={styles.textContent}>The online learning mobile application designed on Figma by Minh Hau and Quynh Giang is a flexible and convenient learning platform. With a friendly interface, the application provides many learning features such as video tutorials, interactive lectures, and tests. Students can easily access and access knowledge anytime, anywhere through this application.</Text>
+        <Text style={styles.textContent}>The online learning mobile application designed on Figma by Ho Minh Hau and Nguyen Thi Quynh Giang is a flexible and convenient learning platform. With a friendly interface, the application provides many learning features such as video tutorials, interactive lectures, and tests. Students can easily access and access knowledge anytime, anywhere through this application.</Text>
       </View>
     </View>
   );
