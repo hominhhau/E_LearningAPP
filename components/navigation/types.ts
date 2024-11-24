76490f7a3166c7111ecc9e681eb050bb5c9f8330
Nguyen QuynhGiang <nguyenquynhgiang1324@gmail.com>
2024-11-25 01:37:12 +0700

    Add file types.ts

---
+++ components/navigation/types.ts
@@ -0,0 +1,10 @@
+export type RootStackParamList ={
+    Setting: undefined;
+    Notification: undefined;
+    SettingGeneralComponent01: undefined;
+    Language: undefined;
+    Legal: undefined;
+    HelpAndSupport: undefined;
+    Security: undefined;
+    SettingGeneralComponent02: undefined;
+}
